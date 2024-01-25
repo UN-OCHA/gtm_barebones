d7c949a78df29f27afc749d6eb4b7f35be60ffb9
Chris Ruppel <ruppel@reliefweb.int>
2024-01-25 11:24:31 +0100

    fix: add max-age and attempt to add cache dependency on container ID
    Refs: CD-515

--- src/Controller/GtmBarebonesController.php
+++ src/Controller/GtmBarebonesController.php
@@ -7,7 +7,7 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Cache\CacheableResponse;
 
 /**
  * Controller for GTM Barebones.
@@ -32,24 +32,31 @@ class GtmBarebonesController extends ControllerBase {
   /**
    * Return inline JS with embedded config.
    *
-   * @return string
+   * @return CacheableResponse
    *   JS to load GTM.
    */
-  public function getJs(): Response {
+  public function getJs(): CacheableResponse {
     $settings = \Drupal::config('gtm_barebones.settings');
     $containerId = $settings->get('container_id');
     $environmentId = $settings->get('environment_id');
     $environmentToken = $settings->get('environment_token');
 
-    $response = new Response(
+    $response = new CacheableResponse(
       'Content',
-      Response::HTTP_OK,
-      ['content-type' => 'text/javascript']
+      CacheableResponse::HTTP_OK,
+      [
+        'content-type' => 'text/javascript',
+        'cache-control' => 'max-age=86400',
+      ]
     );
 
+    // Build JS response with settings embedded.
     $response->setContent(<<<JS
       (function(w,d,s,l,i1,i2,i3){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i1+dl+'&gtm_auth='+i2+'&gtm_preview='+i3+'&gtm_cookies_win=x';var n=d.querySelector('[nonce]');n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);})(window, document, 'script', 'dataLayer', '$containerId', '$environmentToken', '$environmentId');
     JS);
+
+    // Invalidate cache when config changes.
+    $response->addCacheableDependency($containerId);
 
     return $response;
   }
